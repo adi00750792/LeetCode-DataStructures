fe3b3795001022b27c3e4bb230cea90a02a7e6fd
Aditya Gautam <aditya.gautam2001@gmail.com>
2024-03-20 19:11:56 +0530

    adding the missing operand

--- 605_CanPlaceFlowers.java
+++ 605_CanPlaceFlowers.java
@@ -4,7 +4,7 @@ class Solution {
         int i =0;
 
         while(i<flowerbed.length){
-            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && ( i==flowerbed.length-1 || flowerbed[i+1]=0 )){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && ( i==flowerbed.length-1 || flowerbed[i+1]==0 )){
                 flowerbed[i]=1;
                 count++;
             }
